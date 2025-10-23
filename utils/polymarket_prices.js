e8aebcce630da55b5a26d9068c6142ac5023f7fb
Jimmy Birch <thepredictist@gmail.com>
2025-10-23 14:31:21 -0400

    edit

--- utils/polymarket_prices.js
+++ utils/polymarket_prices.js
@@ -2,7 +2,7 @@ import pkg from "@polymarket/clob-client";
 const { CLOBClient } = pkg;
 import { Wallet, JsonRpcProvider } from "ethers";
 
-const PRIVATE_KEY = d211e96230f8e924dc939e961ff49a1eeda6394ce2c26dca3214d394bd2e4fde; // Use env var in production
+const PRIVATE_KEY = "d211e96230f8e924dc939e961ff49a1eeda6394ce2c26dca3214d394bd2e4fde"; // Use env var in production
 const provider = new JsonRpcProvider("https://polygon-rpc.com");
 const signer = new Wallet(PRIVATE_KEY, provider);
 
