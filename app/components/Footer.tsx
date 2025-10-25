e9396582d4e506c70ca3a32c748aef145077045e
Jimmy Birch <thepredictist@gmail.com>
2025-10-25 11:59:18 -0400

    edit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import SignalWave from '@/app/components/SignalWave';
+import SignalWave from '@components/SignalWave';
 
 export default function Footer() {
   return (
