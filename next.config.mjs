50d451216659bb58b2f54aa59357cf37b1653c56
Jimmy Birch <thepredictist@gmail.com>
2025-10-26 19:01:33 -0400

    edit

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const nextConfig = {
   experimental: {
     css: true,
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: ['postcss-loader'],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
