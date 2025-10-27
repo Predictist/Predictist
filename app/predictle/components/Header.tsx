b23be0f347647a6a13209cb5338c95d43d0fad18
Jimmy Birch <thepredictist@gmail.com>
2025-10-27 19:34:18 -0400

    edit

--- app/predictle/components/Header.tsx
+++ app/predictle/components/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-import logo from '@/public/predictle-logo.png';
+import logo from '/predictle-logo.png';
 
 const tabs = [
   { href: '/predictle/free', label: 'Free Play' },
