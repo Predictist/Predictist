dbc661a985d82ccb9558c52b18c6e0160fe81cf7
Jimmy Birch <thepredictist@gmail.com>
2025-10-26 20:27:32 -0400

    edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <NavBar />
-        <main className="container">{children}</main>
-        <Footer />
-      </body>
-    </html>
+  <body className="antialiased bg-[#0A0C10] text-white min-h-screen">
+    <NavBar />
+    <main className="container mx-auto px-6 py-10">{children}</main>
+    <Footer />
+  </body>
+</html>
   );
 }
