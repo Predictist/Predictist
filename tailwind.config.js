65beff8ed34fdec58f3d799f7fa617f4c0577fa2
Jimmy Birch <thepredictist@gmail.com>
2025-10-27 15:26:26 -0400

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
+    safelist: [
+    'bg-blue-600',
+    'text-white',
+    'border-blue-400',
+    'shadow-blue-500/40',
+    'bg-gray-700',
+    'text-gray-300',
+    'border-gray-500',
+    'shadow-gray-500/30',
+  ],
   theme: {
     extend: {},
   },
